5eada9f7a6a0ebecfc3260b7adc6715d66f916bc
NikanorovS <mollyparkeralive@gmail.com>
2023-01-27 14:02:02 +0200

    wrote code for get Date

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 'use strict'
 
-
 let el = document.getElementsByClassName('list_numbers');
 
-for(let i = 0; i < el.length; i++) {
-    el[i].textContent = i + 1;
-}
+function ListNumbers() {
+
+    for(let i = 0; i < el.length; i++) {
+        el[i].textContent = i + 1;
+    }
+    
+};
+
+
+ListNumbers();
+
+let date = new Date();
+
+document.getElementById('date').textContent = 
+date.getDate() + '.' + date.getMonth() + 1 + '.' + date.getFullYear();
