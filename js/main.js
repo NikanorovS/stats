eac9fd19448d3abb49a95b6862aeb1c6032558a5
NikanorovS <mollyparkeralive@gmail.com>
2023-01-28 11:06:44 +0200

    pass +

--- js/main.js
+++ js/main.js
@@ -15,3 +15,18 @@ import getDate from "./getdate.js";
 
 getDate();
 
+/**Пароль */
+
+function passwordCheck() {
+    let pass = prompt("Enter password",'');
+    if (pass === '0000') {
+        document.body.style.display = 'block';
+        return true
+    } else {
+        alert('Password incorrect!')
+        document.body.style.display = 'none';
+        return false
+    }
+}
+
+window.onload = passwordCheck;
