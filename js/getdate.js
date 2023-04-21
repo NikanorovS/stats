56928b5a9eeeff52621f43a745925b9bd312f1a8
NikanorovS <mollyparkeralive@gmail.com>
2023-04-21 17:06:55 +0300

    +

--- js/getdate.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-export default function getDate() {
-    let date = new Date();
-
-    document.getElementById('date').textContent = 'от ' +
-    String(date.getDate()).padStart(2, '0') + '.' + String(date.getMonth() + 1).padStart(2, '0') + '.' + date.getFullYear();
-}
--- js/noselection.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-document.onselectstart = noselect;
-document.ondragstart = noselect;
-document.oncontextmenu = noselect;
-
-export default function noselect() {
-    return false;
-}
