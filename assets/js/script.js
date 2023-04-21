18e7f528270c630c2cb8bccd4c7ef504f7f20459
NikanorovS <mollyparkeralive@gmail.com>
2023-04-21 18:17:10 +0300

    +

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function () {
     	var form = jQuery(this).closest('form');
     	
     	if ( form.valid() ) {
-    		form.css('opacity','.5');
+    		//form.css('opacity','.5');
     		var actUrl = form.attr('action');
 
     		jQuery.ajax({
@@ -16,8 +16,8 @@ jQuery(document).ready(function () {
     			dataType: 'html',
     			data: form.serialize(),
     			success: function(data) {
-    				form.html(data);
-    				form.css('opacity','1');
+    				//form.html(data);
+    				//form.css('opacity','1');
                     form.find('.status').html('форма отправлена успешно');
                     //$('#myModal').modal('show') // для бутстрапа
     			},
