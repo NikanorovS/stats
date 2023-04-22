1375bfb6a775ac6cfdb97a6eea15a7a99237f71f
NikanorovS <mollyparkeralive@gmail.com>
2023-04-22 08:42:44 +0300

    +

--- sender.php
+++ sender.php
@@ -1,25 +1,27 @@
 <?php
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-	$phone = $_POST['phone'];
-    $email = $_POST['email'];
-    $text = $_POST['text'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $message = $_POST['message'];
 
-	$to = "mollyparkeralive@gmail.com"; 
-	$date = date ("d.m.Y"); 
-	$time = date ("h:i");
-	$from = $email;
-	$subject = "Заявка c сайта";
+  $to = 'mollyparkeralive@gmail.com';
+  $subject = 'Новое сообщение с сайта';
+  $message = "Имя: $name\n"
+           . "E-mail: $email\n"
+           . "Телефон: $phone\n"
+           . "Сообщение:\n$message";
 
-	
-	$msg="
-    Имя: $name /n
-    Фамилия: $surname /n
-    Телефон: $phone /n
-    Почта: $email /n
-    Текст: $text"; 	
-	mail($to, $subject, $msg, "From: $to ");
+  $headers = "From: $email\r\n"
+           . "Reply-To: $email\r\n"
+           . "Content-type: text/plain; charset=utf-8\r\n";
 
+  if (mail($to, $subject, $message, $headers)) {
+    http_response_code(200);
+    echo "Ваше сообщение отправлено";
+  } else {
+    http_response_code(500);
+    echo "Что-то пошло не так. Сообщение не отправлено.";
+  }
+}
 ?>
-
-<p>Привет, форма отправлена</p>
